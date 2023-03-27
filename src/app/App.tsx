3ac6734cfe7a705026068bfd8474b61e245b0843
ArtBar9981 <scorp9981@gmail.com>
2023-03-27 11:09:25 +0300

    add modal, portal, change header

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,27 +1,23 @@
-import { Suspense, useEffect } from 'react';
-import './styles/index.scss';
+import React, { Suspense } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
-import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
-import { AppRouter } from './providers/router';
-import { useTheme } from './providers/ThemeProvider';
-import { ErrorBoundary } from './providers/ErrorBoundary';
+import { Sidebar } from 'widgets/Sidebar';
 
 function App() {
     const { theme } = useTheme();
 
     return (
-        <ErrorBoundary>
-            <div className={classNames('app', {}, [theme])}>
-                <Suspense fallback="">
-                    <Navbar />
-                    <div className="content-page">
-                        <Sidebar />
-                        <AppRouter />
-                    </div>
-                </Suspense>
-            </div>
-        </ErrorBoundary>
+        <div className={classNames('app', {}, [theme])}>
+            <Suspense fallback="">
+                <Navbar />
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
+        </div>
     );
 }
 
