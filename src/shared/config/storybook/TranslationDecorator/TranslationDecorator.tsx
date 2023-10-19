d109c205787868b330effa307ae310167fa81573
ArtBar9981 <scorp9981@gmail.com>
2023-10-19 13:53:52 +0300

    added new screenshots

--- src/shared/config/storybook/TranslationDecorator/TranslationDecorator.tsx
+++ src/shared/config/storybook/TranslationDecorator/TranslationDecorator.tsx
@@ -1,11 +1,10 @@
 import { Story } from '@storybook/react';
-import i18next from 'i18next';
 import { Suspense } from 'react';
 import { I18nextProvider } from 'react-i18next';
 import i18nForTests from 'shared/config/i18n/i18nForTests';
 
 export const TranslationDecorator = (StoryComponent: Story) => (
-    <I18nextProvider i18n={i18next}>
+    <I18nextProvider i18n={i18nForTests}>
         <Suspense fallback="">
             <StoryComponent />
         </Suspense>
