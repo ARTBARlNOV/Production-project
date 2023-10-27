a11486f7dd15052cfd9fa18bc6df219d13ac93d7
ArtBar9981 <scorp9981@gmail.com>
2023-10-27 20:18:18 +0300

    fixed types, added profile, fixes tests

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -3,13 +3,11 @@ import { render } from '@testing-library/react';
 import { I18nextProvider } from 'react-i18next';
 import i18nForTests from 'shared/config/i18n/i18nForTests';
 import { MemoryRouter } from 'react-router-dom';
-import { StoreProvider } from 'app/providers/StoreProvider';
-import { DeepPartial } from '@reduxjs/toolkit';
-import { StateSchema } from '../../../../app/providers/StoreProvider/config/StateSchema';
+import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
 
 export interface componentRenderOptions {
     route?: string;
-    initialState?: DeepPartial<StateSchema>
+    initialState?: DeepPartial<StateSchema>;
 }
 
 export function componentRender(component: ReactNode, options: componentRenderOptions = {}) {
@@ -19,12 +17,12 @@ export function componentRender(component: ReactNode, options: componentRenderOp
     } = options;
 
     return render(
-        <StoreProvider initialState={initialState}>
-            <MemoryRouter initialEntries={[route]}>
+        <MemoryRouter initialEntries={[route]}>
+            <StoreProvider initialState={initialState}>
                 <I18nextProvider i18n={i18nForTests}>
                     {component}
                 </I18nextProvider>
-            </MemoryRouter>
-        </StoreProvider>,
+            </StoreProvider>
+        </MemoryRouter>,
     );
 }
